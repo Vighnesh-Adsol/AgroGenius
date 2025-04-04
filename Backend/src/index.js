e168b6167b5cb007d101e7627500e18055192f8d
Vighnesh-Adsol <vighneshadsol29@gmail.com>
2025-04-04 16:38:02 +0530

    deployement

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,6 +3,10 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import cors from 'cors'
+
+import path from "path"
+
+
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser"
 import {connectDB} from "./lib/db.js"
@@ -18,11 +22,21 @@ app.use(cors({
 }
 ))
 const PORT = process.env.PORT
+const __dirname = path.resolve();
 
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../Frontend", "dist", "index.html"));
+    });
+}
+
 server.listen(PORT, ()=>{
     console.log("Server is running on port: "+PORT);
     connectDB();
